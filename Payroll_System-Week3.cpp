a66b417287e8fde8309aa92f7057281e5ccc28fa
maxwell-nu <09kofigox@proton.me>
2026-05-29 17:05:27 +0000

    Add files via upload

---
+++ Payroll_System-Week3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int overtimeHours, salary, overtimePay, rph, netSalary;
+string name;
+
+void getEmployeeDetails(){
+    cout << "Enter Employee Name: ";
+    getline(cin, name);
+    cout << "Enter Basic Salary: ";
+    cin >> salary;
+    cout << "Enter Overtime Hours: ";
+    cin >> overtimeHours;
+}
+
+void calculateOvertimePay(){
+    rph = 10;
+    overtimePay = overtimeHours * rph;
+}
+
+void calculateNetSalary(){
+    netSalary = salary + overtimePay;
+}
+
+void displayPayslip(){
+    cout << "Employee Name: " << name << endl;
+    cout << "Basic Salary: " << salary << endl;
+    cout << "Overtime Hours: " << overtimeHours << endl;
+    cout << "Overtime Pay: " << overtimePay << endl;
+    cout << "Rate per hour: " << rph << endl;
+    cout << "Net Salary: " << netSalary << endl;
+}
+
+int main(){
+    getEmployeeDetails();
+    calculateOvertimePay();
+    calculateNetSalary();
+    displayPayslip();
+
+    return 0;
+}
