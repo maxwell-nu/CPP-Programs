e1b0da1ec60cb998c240e8b80f1ec887b49a09b1
maxwell-nu <09kofigox@proton.me>
2026-06-11 13:57:00 +0300

    Add files via upload

---
+++ library_book_management_system-Week6.cpp
@@ -0,0 +1,47 @@
+//Name: Maxwell Mwangi
+//Reg No: BCS-05-0001/2025
+//Purpose: Library books management system
+
+#include <iostream>
+using namespace std;
+
+class Book {
+
+    public:
+    string bookTitle, author;
+    int availableCopies, available, borrowed;
+
+
+    void inputDetails(){
+
+        cout << "Enter Book Title: ";
+        getline(cin, bookTitle);
+        cout << "Enter Author: ";
+        getline(cin, author);
+    };
+
+    int borrowBook(){
+         
+        availableCopies = 20;
+        cout << "How many copies would you like to borrow(1-20): ";
+        cin >> borrowed;
+        return available = availableCopies - borrowed;
+    };
+
+    void displayDetails(){
+
+        cout << "\nBook Title: " << bookTitle << endl
+        << "Author: " << author << endl << "Borrowed: " << borrowed << endl << "Available Copies: " << available << endl;
+    };
+
+};
+
+int main(){
+
+    Book book_obj;
+    book_obj.inputDetails();
+    book_obj.borrowBook();
+    book_obj.displayDetails();
+        
+    return 0;
+}
