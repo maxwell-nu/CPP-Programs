54069fe9be9eea7adaefe2e9a197864f385a8d06
maxwell-nu <09kofigox@proton.me>
2026-06-13 10:44:36 +0300

    Add files via upload

---
+++ student_fee_management_system-Week6.cpp
@@ -0,0 +1,43 @@
+//Name: Maxwell Mwanngi
+//Reg No: BCS-05-0001/2025
+//Purpose: A fee management system using classes and objects. Fee is set to 100000.
+
+#include <iostream>
+using namespace std;
+int payment, remainingFee;
+
+class Student{
+    public:
+    string studentName, admissionNumber;
+    int feeBalance = 100000;
+
+    void inputStudent(){
+        cout << "Enter Student Name: ";
+        getline(cin, studentName);
+        cout << "Enter Admission Number: ";
+        getline(cin, admissionNumber);
+        cout << "Enter Fee Payment Amount: ";
+        cin >> payment;
+    }
+
+    int makePayment(){
+        return remainingFee = feeBalance - payment;
+    }
+
+    void displayStatus(){
+        cout << "\nStudent Name: " << studentName << endl
+        << "Admission Number: " << admissionNumber << endl
+        << "Remaining Fee Balance: " << remainingFee;
+    }
+
+};
+
+int main(){
+
+        Student StudentObj;
+        StudentObj.inputStudent();
+        StudentObj.makePayment();
+        StudentObj.displayStatus();
+
+        return 0;
+    };
