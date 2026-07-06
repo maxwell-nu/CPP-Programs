e2f1a4eead8cb5f3da1dac6a7307ab6a9a727bde
maxwell-nu <09kofigox@proton.me>
2026-07-06 12:49:48 +0300

    Add files via upload

---
+++ Library_Management_System-Assignment.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+class Person{
+    protected:
+    string name;
+
+    public:
+    void setName(string n){
+        name = n;
+    }
+    string getName(){
+        return name;
+    }
+};
+
+class LibraryMember: public Person{
+    private:
+    int memberID;
+    int booksBorrowed;
+
+    public:
+    LibraryMember(string n, int memID, int borBooks){
+        name = n;
+        memberID = memID;
+        booksBorrowed = borBooks;
+    }
+
+    int getMemberID(){
+        return memberID;
+    }
+    int getBooksBorrowed(){
+        return booksBorrowed;
+    }
+    
+};
+
+class PremiumMember: public LibraryMember{
+    private:
+    double membershipFee;
+
+    public:
+    PremiumMember(string n, int memID, int borBooks, double memFee) : LibraryMember(n, memID, borBooks){
+        membershipFee = memFee;
+    }
+
+    double getMembershipFee(){
+        return membershipFee;
+    }
+};
+
+int main(){
+
+    PremiumMember vip("John Doe", 626, 5, 40);
+    cout << "Name: " << vip.getName() << endl
+    << "Membership ID: " << vip.getMemberID() << endl
+    << "Borrowed Books: " << vip.getBooksBorrowed() << endl
+    << "Membership Fee: £" << vip.getMembershipFee();
+
+    return 0;
+}
